02ce77418b25dec65c3abd47807a398085600419
taharon <taharon1990@gmail.com>
2020-05-15 12:38:01 -0400

    ACTUAL correct heroku link

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://gentle-badlands-53737.herokuapp.com/',
+  production: 'https://gentle-badlands-53737.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
